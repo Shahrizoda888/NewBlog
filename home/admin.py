dafe8066f0770be865ff7a737e835ca47d227844
Shahrizoda <87145658+Shahrizoda888@users.noreply.github.com>
2021-11-21 02:05:38 +0500

    21

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import BlogModel
 # Register your models here.
+admin.site.register(BlogModel)
--- home/models.py
+++ home/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 from froala_editor.fields import FroalaField
+from .helpers import *
 
 
 #blog
-class Blogmodel(models.Model):
+class BlogModel(models.Model):
     title=models.CharField(max_length=1000)
     content=FroalaField()
-    slug=models.SlugField(max_length=1000)
+    slug=models.SlugField(max_length=1000, null=True,blank=True)
     image=models.ImageField(upload_to='blog')
     created_at=models.DateTimeField(auto_now_add=True)
-    upload_to=models.DateTimeField(auto_now=True)
+    upload_to=models.DateTimeField(auto_now=True) 
+
+
+    def __str__(self):
+        return self.title
+
+
+    def save(self):
+        
+        self.slug=generate_slug(self.title)
+        super(BlogModel, self).save(*args, **kwargs)
